2534e2b60eb26f97a31943cf1159df28c147741f
anyi015 <anyimo3105@gmail.com>
2023-11-25 23:33:54 -0600

    Datos de informes

--- src/components/informes.js
+++ src/components/informes.js
@@ -1,40 +1,37 @@
 import React, { useState, useEffect } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
-import { db, auth} from '../Api/firebaseConfig';
+import { db, auth } from '../Api/firebaseConfig';
 import Gastos from './Gastos';
 
 export function Informes() {
   const [gastos, setGastos] = useState([]);
-  
+  const [objetivos, setObjetivos] = useState([]);
   const [cuentas, setCuentas] = useState([]);
   const [ingresos, setIngresos] = useState([]);
-  const navigate = useNavigate();  
-  
+  const navigate = useNavigate();
+
   const user = auth.currentUser
   const userId = user ? user.uid : null;
-  
+
   useEffect(() => {
     const user = auth.currentUser;
 
     if (!user) {
-        // Redirige al usuario a la página de inicio de sesión si no está autenticado
-        navigate('/Informes');
-        return;
+      // Redirige al usuario a la página de inicio de sesión si no está autenticado
+      navigate('/Informes');
+      return;
     }
-   
+
 
     const userId = user.uid;
-    const unsubscribeGastos = db
-    .collection('usuarios')
-    .doc(userId)
-    .collection('gastos')
-    .onSnapshot((snapshot) => {
+    const unsubscribeGastos = db.collection('usuarios').doc(userId).collection('gastos').onSnapshot((snapshot) => {
       const nuevosGastos = snapshot.docs.map((doc) => ({
         id: doc.id,
-        fechaGasto: doc.data().fechaGasto,
-        gasto: doc.data().gasto,
-        valorGasto: doc.data().valorGasto,
+        saldoA: doc.data().saldoA,
         cuentaInstitucion: doc.data().cuentaInstitucion,
+        valor: doc.data().valor,
+        valorGasto: doc.data().valorGasto,
+        valorObjetivo: doc.data().valorObjetivo,
       }));
       setGastos(nuevosGastos);
     });
@@ -75,21 +72,38 @@ export function Informes() {
       };
     }
   }, [userId]);
-  
+  useEffect(() => {
+    if (userId) {
+      const unsubscribeObjetivos = db
+        .collection('usuarios')
+        .doc(userId)
+        .collection('objetivos')
+        .onSnapshot((snapshot) => {
+          const nuevosObjetivos = snapshot.docs.map((doc) => ({
+            id: doc.id,
+            ...doc.data(),
+          }));
+          setObjetivos(nuevosObjetivos);
+        });
 
-  // Función para sumar los ingresos por cuenta
-  const obtenerTotalIngresosPorCuenta = (cuenta) => {
-    // Filtra los ingresos por la cuenta actual
-    const ingresosDeCuenta = ingresos.filter(
-      (ingreso) => ingreso.cuentaInstitucion === cuenta.institucion
-    );
+      return () => {
+        unsubscribeObjetivos();
+      };
+    }
+  }, [userId]);
 
-    // Suma los valores de los ingresos
-    const total = ingresosDeCuenta.reduce((suma, ingreso) => suma + (ingreso.valor || 0), 0);
 
-    return total;
+  // Función para filtrar ingresos por cuenta
+  const obtenerIngresosPorCuenta = (cuenta) => {
+    return ingresos.filter((ingreso) => ingreso.cuentaInstitucion === cuenta.institucion);
   };
-    
+
+  // Función para filtrar gastos por cuenta
+  const obtenerGastosPorCuenta = (cuenta) => {
+    console.log('datos', gastos);
+    return gastos.filter((gasto) => gasto.cuentaInstitucion === cuenta.institucion);
+  };
+
   return (
     <section>
       <div className="container">
@@ -103,149 +117,80 @@ export function Informes() {
           </div>
         </div>
 
-{/* <div class="row row-cols-1 row-cols-md-2 row-cols-xl-4">
-       <div class="col">
-     <div class="card radius-10 border-start border-0 border-3 border-info">
-      <div class="card-body">
-        <div>
-          <div class="d-flex align-items-center" >
-          <div>
-          <p className="mb-0 text-secondary">
-                    <b>Total ingresos por cuenta:</b>
-                  </p>
-                  <br />
-                  {cuentas.map((cuenta) => (
-                    <div key={cuenta.id}>
-                      <p className="mb-0">
-                        <b>
-                        {cuenta.institucion}
-                        </b>
-                        </p>
-                      <ul>
-                        {ingresos
-                          .filter((ingreso) => ingreso.cuentaInstitucion === cuenta.institucion)
-                          .map((ingreso) => (
-                            <li key={ingreso.id}>
-                              ${ingreso.valor}
-                              <hr />
-                              </li>
-                          ))}
-                      </ul>
-                    </div>
-                  ))}
-                </div>
-              </div>
-          <div class="widgets-icons-2 rounded-circle bg-gradient-scooter text-white ms-auto"><i class="fa fa-dollar"></i>
-          </div>
-        </div>
-        
-      </div>
-     </div>
-     </div>
-     <div class="col">
-
-    <div class="card radius-10 border-start border-0 border-3 border-danger">
-       <div class="card-body">
-        <div>
-         <div class="d-flex align-items-center">
-           <div>
-             <p class="mb-0 text-secondary"><b>Total Gastos por cuenta:</b></p>
-             <br></br>
-
-             {cuentas.map((cuenta) => (
-                    <div key={cuenta.id}>
-                      <p className="mb-0">
-                        <b>
-                        {cuenta.institucion}
-                        </b>
-                        </p>
-                      <ul>
-                        {gastos
-                          .filter((gasto) => gasto.cuentaInstitucion === gasto.institucion)
-                          .map((gasto) => (
-                            <li key={gasto.id}>
-                              ${gasto.valorGasto}
-                              <hr />
-                              </li>
-                          ))}
-                      </ul>
-                    </div>
-                  ))}
-            
-           </div>
-           <div class="widgets-icons-2 rounded-circle bg-gradient-bloody text-white ms-auto"><i class="fa-solid fa-money-bill-trend-up"></i>
-           </div>
-         </div>
-         </div>
-
-       </div>
-    </div>
-    </div>
-    </div> */}
- 
- 
- <div className="row align-items-left mt-4">
+        <div className="row align-items-left mt-4">
           <div className="col">
+            <h2><b>Ingresos y Gastos por Cuenta:</b></h2>
+            <br></br>
             <table className="table table-hover">
               <thead>
                 <tr className="table-success">
                   <th>Cuenta</th>
-                  <th>Total Ingresos</th>
+                  <th>Ingreso</th>
+                  <th>Gasto</th>
                 </tr>
               </thead>
               <tbody>
                 {cuentas.map((cuenta) => (
                   <tr key={cuenta.id}>
                     <td>{cuenta.institucion}</td>
-                    <td>${obtenerTotalIngresosPorCuenta(cuenta)}</td>
+                    <td>
+                      {obtenerIngresosPorCuenta(cuenta).map((ingreso) => (
+                        <div key={`ingreso_${ingreso.id}`}>${ingreso.valor}</div>
+                      ))}
+                    </td>
+                    <td>
+                      {obtenerGastosPorCuenta(cuenta).map((gasto) => (
+                        <div key={`gasto_${gasto.id}`}>${gasto.valorGasto}
+                      
+                        </div>
+                      ))}
+                    </td>
                   </tr>
                 ))}
               </tbody>
             </table>
           </div>
-        </div>
 
-        <div className="row align-items-left mt-4">
           <div className="col">
-            <h2>Total de Ingresos por Cuenta</h2>
+            <br></br>
+
+            <h2><b>Objetivos:</b></h2>
+            <br></br>
             <table className="table table-hover">
               <thead>
-                <tr className="table-success">
-                  <th>Cuenta</th>
-                  <th>Total Ingresos</th>
+                <tr className="table-danger">
+                  <th>Nombre del Objetivo</th>
+                  <th>Fecha de Inicio</th>
+                  <th>Fecha de Finalización</th>
                 </tr>
               </thead>
               <tbody>
-                {cuentas.map((cuenta) => (
-                  <tr key={cuenta.id}>
-                    <td>{cuenta.institucion}</td>
-                    <td>${obtenerTotalIngresosPorCuenta(cuenta)}</td>
+                {objetivos.map((objetivo) => (
+                  <tr key={objetivo.id}>
+                    <td>{objetivo.objetivo}</td>
+                    <td>{objetivo.fechaInicio}</td>
+                    <td>{objetivo.fechaFinal}</td>
+                    {/* Agrega más celdas según tus necesidades */}
                   </tr>
                 ))}
               </tbody>
             </table>
           </div>
+
+
+
+
         </div>
 
-        <div className="row align-items-left mt-4">
-          <div className="col">
-            <h2>Total General</h2>
-            <p>
-              Total de Ingresos: ${ingresos.reduce((total, ingreso) => total + (ingreso.valor || 0), 0)}
-            </p>
-            <p>
-              Total de Gastos: ${gastos.reduce((total, gasto) => total + (gasto.valorGasto || 0), 0)}
-            </p>
-          </div>
-        </div>
+
       </div>
+
     </section>
 
 
 
 
 
-    
   );
 }
 export default Informes;
